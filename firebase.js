a728ddeec68ed291dc91a254725d0423e67a3ae7
Jamesxtmp <jamesxtmp@gmail.com>
2025-11-07 17:39:58 -0600

    Update build script to include Firebase deployment and add Firebase configuration file

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAHfQZGf4wz9e5-FwZY2XV7rfiZfZvpczU",
+  authDomain: "xtmp-portafolio.firebaseapp.com",
+  databaseURL: "https://xtmp-portafolio-default-rtdb.firebaseio.com",
+  projectId: "xtmp-portafolio",
+  storageBucket: "xtmp-portafolio.firebasestorage.app",
+  messagingSenderId: "827537104187",
+  appId: "1:827537104187:web:1c0d1c3fc4b5fa3f65eb1e",
+  measurementId: "G-K6QHSTJZR3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
